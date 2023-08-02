ef0bda38487d76af373161cbc9e289b70bd35c63
Chisom <uchisom10@gmail.com>
2023-08-02 10:05:13 -0700

    Did some bug fixes on the input component of the calculator

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ for (let key of keys) {
         }
         else if (value == 'backspace') {
             input = input.slice(0, -1);
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         }
         else if (value == '=') {
             let result = eval(input);
@@ -40,10 +40,35 @@ for (let key of keys) {
                 input += ')'
             }
 
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         } else {
             input += value;
-            display_input.innerHTML = input;
+            display_input.innerHTML = CleanInput(input);
         }
     })
+}
+
+const CleanInput = (input) => {
+    let input_array = input.split("");
+    let input_array_length = input_array.length;
+
+    for (let i = 0; i < input_array_length; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = `<span class="operator">X</span>`;
+        } else if (input_array[i] == "/") {
+            input_array[i] = `<span class="operator">÷</span>`;
+        } else if (input_array[i] == "+") {
+            input_array[i] = `<span class="operator">+</span>`;
+        } else if (input_array[i] == "-") {
+            input_array[i] = `<span class="operator">-</span>`;
+        } else if (input_array[i] == "(") {
+            input_array[i] = `<span class="brackets">(</span>`;
+        } else if (input_array[i] == ")") {
+            input_array[i] = `<span class="brackets">)</span>`;
+        } else if (input_array[i] == "%") {
+            input_array[i] = `<span class="percent">%</span>`;
+        }
+    }
+
+    return input_array.join("");
 }
